ed37bc602de2799c586e7940bccfe08fe8bb771b
SHOBHIT GUPTA <145250483+ShobhitGupta1512@users.noreply.github.com>
2025-09-15 23:21:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
->>>>>>> d8e7f60bdcde2962b3db903e5078d2bea8154acb
+  base: "./", // helps when deploying to Vercel/Netlify
+});
